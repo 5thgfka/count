31a7ce91de564c744779fd4de4817fc8b02aedab
ekse <5thgfka@gmail.com>
2012-08-28 01:49:20 +0800

    new file:   .classpath 	new file:   .project 	new file:   AndroidManifest.xml 	new file:   bin/jarlist.cache 	new file:   gen/com/count/BuildConfig.java 	new file:   gen/com/count/R.java 	new file:   proguard-project.txt 	new file:   project.properties 	new file:   res/drawable-hdpi/ic_launcher.png 	new file:   res/drawable-ldpi/ic_launcher.png 	new file:   res/drawable-mdpi/ic_launcher.png 	new file:   res/drawable-xhdpi/ic_launcher.png 	new file:   res/layout/main.xml 	new file:   res/values/strings.xml 	new file:   src/com/count/Count.java

---
+++ src/com/count/Count.java
@@ -0,0 +1,17 @@
+package com.count;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * 天下事渺茫如此，古今人大概相同。
+ * @author ekse
+ *
+ */
+public class Count extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
